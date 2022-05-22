263a7344c75da037cacf73e3eb1e32266308aaaf
Kanwar Zorawar Singh Rana <xorture@gmail.com>
2022-05-21 22:03:16 -0400

    Updating Package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UIEngine",
-            dependencies: ["Sources"]),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "UIEngineTests",
             dependencies: ["UIEngine"]),
